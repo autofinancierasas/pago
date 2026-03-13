9478daa8191876d10c962bc34b97cbbec1b5dd9d
autofinancierasas <wiinstoncc1995@gmail.com>
2026-03-13 13:53:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-document.getElementById("paymentForm").addEventListener("submit", function(e){
+document.getElementById("formPago").addEventListener("submit", function(e){
 
 e.preventDefault();
 
-const referencia=document.getElementById("referencia").value;
-const concepto=document.getElementById("concepto").value;
-const valor=document.getElementById("valor").value;
-const email=document.getElementById("email").value;
-const nombre=document.getElementById("nombre").value;
-const cedula=document.getElementById("cedula").value;
-const celular=document.getElementById("celular").value;
+const referencia = document.getElementById("referencia").value;
+const concepto = document.getElementById("concepto").value;
+const valor = document.getElementById("valor").value;
+const email = document.getElementById("email").value;
+const nombre = document.getElementById("nombre").value;
+const cedula = document.getElementById("cedula").value;
+const celular = document.getElementById("celular").value;
 
-const params=new URLSearchParams({
-referencia,concepto,valor,email,nombre,cedula,celular
+const url = `verificar.html?referencia=${encodeURIComponent(referencia)}
+&concepto=${encodeURIComponent(concepto)}
+&valor=${encodeURIComponent(valor)}
+&email=${encodeURIComponent(email)}
+&nombre=${encodeURIComponent(nombre)}
+&cedula=${encodeURIComponent(cedula)}
+&celular=${encodeURIComponent(celular)}`;
+
+window.location.href = url;
+
 });
-
-window.location.href="verificar.html?"+params.toString();
-
-});;
