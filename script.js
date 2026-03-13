ec8298d45c60d402a439998bd7906db7b4dc5dcc
autofinancierasas <wiinstoncc1995@gmail.com>
2026-03-13 13:48:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-document.getElementById("formPago").addEventListener("submit", function(e){
-    e.preventDefault();
+document.getElementById("paymentForm").addEventListener("submit", function(e){
 
-    const referencia = document.getElementById("referencia").value;
-    const concepto = document.getElementById("concepto").value;
-    const valor = document.getElementById("valor").value;
-    const email = document.getElementById("email").value;
-    const nombre = document.getElementById("nombre").value;
-    const cedula = document.getElementById("cedula").value;
-    const celular = document.getElementById("celular").value;
+e.preventDefault();
 
-    const params = new URLSearchParams({
-        referencia, concepto, valor, email, nombre, cedula, celular
-    });
+const referencia=document.getElementById("referencia").value;
+const concepto=document.getElementById("concepto").value;
+const valor=document.getElementById("valor").value;
+const email=document.getElementById("email").value;
+const nombre=document.getElementById("nombre").value;
+const cedula=document.getElementById("cedula").value;
+const celular=document.getElementById("celular").value;
 
-    window.location.href = "verificar.html?" + params.toString();
+const params=new URLSearchParams({
+referencia,concepto,valor,email,nombre,cedula,celular
 });
+
+window.location.href="verificar.html?"+params.toString();
+
+});;
