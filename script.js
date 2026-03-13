3c569af558bd431da919ba7843386629ecbd2baf
autofinancierasas <wiinstoncc1995@gmail.com>
2026-03-13 12:49:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 document.getElementById("formPago").addEventListener("submit", function(e){
+    e.preventDefault();
 
-e.preventDefault();
+    const referencia = document.getElementById("referencia").value;
+    const concepto = document.getElementById("concepto").value;
+    const valor = document.getElementById("valor").value;
+    const email = document.getElementById("email").value;
+    const nombre = document.getElementById("nombre").value;
+    const cedula = document.getElementById("cedula").value;
+    const celular = document.getElementById("celular").value;
 
-const referencia = document.getElementById("referencia").value;
-const concepto = document.getElementById("concepto").value;
-const valor = document.getElementById("valor").value;
-const email = document.getElementById("email").value;
-const nombre = document.getElementById("nombre").value;
-const cedula = document.getElementById("cedula").value;
-const celular = document.getElementById("celular").value;
+    const params = new URLSearchParams({
+        referencia, concepto, valor, email, nombre, cedula, celular
+    });
 
-const params = new URLSearchParams();
-
-params.append("referencia", referencia);
-params.append("concepto", concepto);
-params.append("valor", valor);
-params.append("email", email);
-params.append("nombre", nombre);
-params.append("cedula", cedula);
-params.append("celular", celular);
-
-window.location.href = "verificar.html?" + params.toString();
-
+    window.location.href = "verificar.html?" + params.toString();
 });
